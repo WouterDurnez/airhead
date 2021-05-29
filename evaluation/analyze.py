8a37d542a50ed6eeba4a152fc49d76b31668d20c
wouter <wouter.durnez@gmail.com>
2021-05-29 22:44:46 +0200

    Small tweak for comparison

--- evaluation/analyze.py
+++ evaluation/analyze.py
@@ -3,49 +3,54 @@ from os.path import join
 import numpy as np
 
 import utils.helper as hlp
+from utils.helper import log
 
 if __name__ == '__main__':
     hlp.hi('Analysis')
 
     model_name = 'unet_baseline'
-    version = 3
 
-    result_dir = join(hlp.LOG_DIR, 'results', model_name)
+    for version in (3,5,6):
+        #version = 6
 
-    results = np.load(join(result_dir, f'{model_name}_v{version}.npy'), allow_pickle=True)
+        log(f'version {version}',color='cyan')
 
-    dice = []
-    dice_et = []
-    dice_tc = []
-    dice_wt = []
-    hd = []
-    hd_et = []
-    hd_tc = []
-    hd_wt = []
+        result_dir = join(hlp.LOG_DIR, 'results', model_name)
 
-    best = 0
-    id = None
+        results = np.load(join(result_dir, f'{model_name}_v{version}.npy'), allow_pickle=True)
 
-    for res in results:
+        dice = []
+        dice_et = []
+        dice_tc = []
+        dice_wt = []
+        hd = []
+        hd_et = []
+        hd_tc = []
+        hd_wt = []
 
-        if res['test_dice_metric'] > best:
-            id = res['id']
+        best = 0
+        id = None
 
-        dice.append(res['test_dice_metric'])
-        dice_et.append(res['test_dice_et'])
-        dice_tc.append(res['test_dice_tc'])
-        dice_wt.append(res['test_dice_wt'])
-        hd.append(res['test_hd_metric'])
-        hd_et.append(res['test_hd_et'])
-        hd_tc.append(res['test_hd_tc'])
-        hd_wt.append(res['test_hd_wt'])
+        for res in results:
 
-    print('dice: ', np.mean(dice))
-    print('dice et: ', np.mean(dice_et))
-    print('dice tc: ', np.mean(dice_tc))
-    print('dice wt: ', np.mean(dice_wt))
+            if res['test_dice_metric'] > best:
+                id = res['id']
 
-    print('hd: ', np.mean(hd))
-    print('hd et: ', np.mean(hd_et))
-    print('hd tc: ', np.mean(hd_tc))
-    print('hd wt: ', np.mean(hd_wt))
+            dice.append(res['test_dice_metric'])
+            dice_et.append(res['test_dice_et'])
+            dice_tc.append(res['test_dice_tc'])
+            dice_wt.append(res['test_dice_wt'])
+            hd.append(res['test_hd_metric'])
+            hd_et.append(res['test_hd_et'])
+            hd_tc.append(res['test_hd_tc'])
+            hd_wt.append(res['test_hd_wt'])
+
+        print('dice: ', np.mean(dice))
+        print('dice et: ', np.mean(dice_et))
+        print('dice tc: ', np.mean(dice_tc))
+        print('dice wt: ', np.mean(dice_wt))
+
+        print('hd: ', np.mean(hd))
+        print('hd et: ', np.mean(hd_et))
+        print('hd tc: ', np.mean(hd_tc))
+        print('hd wt: ', np.mean(hd_wt))
