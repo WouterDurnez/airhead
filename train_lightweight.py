d5dc7ddbb0bde0ace7e44ad069fe42453dc5217e
wouter <wouter.durnez@gmail.com>
2021-06-02 16:53:44 +0200

    Add lightweight training script

---
+++ train_lightweight.py
@@ -0,0 +1,155 @@
+#     ___   _     __               __
+#    / _ | (_)___/ /  ___ ___ ____/ /
+#   / __ |/ / __/ _ \/ -_) _ `/ _  /
+#  /_/ |_/_/_/ /_//_/\__/\_,_/\_,_/
+#
+
+"""
+Main training script for lightweight Unet
+"""
+
+from os.path import join
+from pprint import PrettyPrinter
+
+import numpy as np
+import torch.cuda
+from pl_bolts.callbacks import PrintTableMetricsCallback
+from ptflops import get_model_complexity_info
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch import optim
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from models.unet import UNet
+from lightweight.lightweight_unet import LowRankUNet
+from models.unet_lightning import UNetLightning
+from training.data_module import BraTSDataModule
+from training.inference import val_inference, test_inference
+from training.losses import dice_loss, dice_metric, dice_et, dice_tc, dice_wt, hd_metric, hd_et, hd_tc, hd_wt
+from utils import helper as hlp
+from utils.helper import log
+from utils.helper import set_dir
+from utils.utils import WarmupCosineSchedule, TakeSnapshot
+from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+
+pp = PrettyPrinter()
+
+if __name__ == '__main__':
+    # Let's go
+    hlp.hi("Training baseline U-Net")
+
+    # Name
+    model_name = 'unet_canon'
+    version = 0
+
+    # Parameters
+    compression = 50
+    tensor_net_type = 'cpd'
+
+    # Set data directory
+    data_dir = hlp.DATA_DIR
+    tb_dir = join(hlp.LOG_DIR, 'tb_logs')
+    snap_dir = join(hlp.LOG_DIR, 'snapshots', model_name)
+    result_dir = join(hlp.LOG_DIR, 'results', model_name)
+    #model_dir = join(hlp.LOG_DIR, 'models', model_name)
+    set_dir(data_dir, tb_dir, snap_dir, result_dir)
+
+    # Initialize model
+    log(f"Initializing <{model_name}> model")
+    model = UNetLightning(
+
+        # Architecture settings
+        network=LowRankUNet,
+        network_params={
+            'compression': compression,
+            'tensor_net_type': tensor_net_type,
+            'in_channels': 4,
+            'out_channels': 3,
+            'widths': (32, 64, 128, 256, 320),
+            'head': False},
+
+        # Loss and metrics
+        loss=dice_loss,
+        metrics=[dice_metric, dice_et, dice_tc, dice_wt,
+                 hd_metric, hd_et, hd_tc, hd_wt],
+
+        # Optimizer
+        optimizer=optim.AdamW,
+        optimizer_params={'lr': 1e-4, 'weight_decay': 1e-5},
+
+        # Learning rate scheduler
+        #scheduler=WarmupCosineSchedule,
+        scheduler=CosineAnnealingWarmRestarts,
+        scheduler_config={'interval': 'epoch'},
+        #scheduler_params={'warmup_steps': 3*3e2, 'total_steps': 1e5},
+        #scheduler_params={'warmup_steps': 294*5, 'total_steps': 1e5},
+        #scheduler_params={'warmup_epochs': 1, 'max_epochs':150},
+        scheduler_params={'T_0': 50, 'eta_min':3e-5},
+
+        # Inference method
+        inference=val_inference,
+        inference_params=None,
+
+        # Test inference method
+        test_inference=test_inference,
+        test_inference_params=None,
+    )
+
+    # Load checkpoint
+    #model.load_from_checkpoint(checkpoint_path=join(snap_dir,f'final_{model_name}_v0.ckpt'))
+
+    # Initialize data module
+    log("Initializing data module")
+    brats = BraTSDataModule(data_dir=join(data_dir,"MICCAI_BraTS2020_TrainingData"),
+                            num_workers=8,
+                            batch_size=1,
+                            validation_size=.2)
+    brats.setup()
+
+    # Initialize logger
+    tb_logger = TensorBoardLogger(save_dir=tb_dir, name=model_name, default_hp_metric=False, version=version)
+
+    # Initialize trainer
+    log("Initializing trainer")
+    trainer = Trainer(
+        max_steps=100000,
+        max_epochs=150,
+        logger=tb_logger,
+        gpus=1,
+        num_nodes=1,
+        deterministic=True,
+        # distributed_backend='ddp',
+        callbacks=[
+            LearningRateMonitor(logging_interval="step"),
+            PrintTableMetricsCallback(),
+            #TakeSnapshot(epochs=(1, 24, 49), save_dir=snap_dir)
+        ],
+    )
+
+    # Train
+    log("Commencing training")
+    trainer.fit(model=model,
+                datamodule=brats)
+
+    # Additional checkpoint (just in case)
+    trainer.save_checkpoint(join(snap_dir, f'final_{model_name}_v{version}.ckpt'))
+
+    # Test
+    #torch.cuda.empty_cache()
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    log("Evaluating model")
+    trainer.test()
+    results = model.test_results
+
+    # Adding model parameters
+    with torch.cuda.device(0):
+        macs, params = get_model_complexity_info(model=model, input_res=(4, 128, 128, 128), as_strings=True,
+                                                 print_per_layer_stat=True, verbose=False)
+
+    eval = {'results': results,
+            'n_param': model.get_n_parameters(),
+            'flops_count': macs,
+            'params': params}
+
+    # Save test results
+    np.save(file=join(result_dir, f'{model_name}_v{version}.npy'), arr=results)
