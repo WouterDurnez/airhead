31ebd2b0a53b0ece524b4595cf70ac2a7ee82cfc
wouter <durnez47@imec.be>
2021-06-10 10:40:37 +0200

    Add hpc test

--- hpc_test.py
+++ hpc_test.py
@@ -1,7 +1,10 @@
 import utils.helper as h
 import os
+import torch
+from utils.helper import log
+from layers import lightweight_conv
 
 if __name__ == '__main__':
 
     h.hi('HPC test')
-    print(os.listdir(h.DATA_DIR))
+    log(torch.cuda.device_count(), color='red')
