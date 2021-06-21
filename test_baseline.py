12af0e4b9393431fc26346c0750d9a695219c643
wouter <durnez47@imec.be>
2021-06-21 19:43:10 +0200

    Add test script for baseline networks

--- test_baseline.py
+++ test_baseline.py
@@ -7,44 +7,52 @@
 """
 Main training script
 """
-
+import argparse
 from os.path import join
+from pprint import PrettyPrinter
+
 import numpy as np
 import torch.cuda
-from pytorch_lightning import Trainer
 from pl_bolts.callbacks import PrintTableMetricsCallback
+from ptflops import get_model_complexity_info
+from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor
-from torch import optim
 from pytorch_lightning.loggers import TensorBoardLogger
-
-from utils import helper as hlp
-from utils.helper import log
+from torch import optim
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
 from models.baseline_unet import UNet
 from training.lightning import UNetLightning
 from training.data_module import BraTSDataModule
 from training.inference import val_inference, test_inference
 from training.losses import dice_loss, dice_metric, dice_et, dice_tc, dice_wt, hd_metric, hd_et, hd_tc, hd_wt
-from utils.utils import WarmupCosineSchedule, TakeSnapshot
+from utils import helper as hlp
+from utils.helper import log
 from utils.helper import set_dir
-from pprint import PrettyPrinter
-from ptflops import get_model_complexity_info
 
 pp = PrettyPrinter()
 
 if __name__ == '__main__':
     # Let's go
-    hlp.hi("Training baseline U-Net")
+    hlp.hi("Training baseline U-Net", log_dir='../logs_cv')
 
-    # Name
-    model_name = 'unet_baseline'
+    # Get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fold', help='Fold index')
+    args = parser.parse_args()
+
+    log(f'Fold index:    {args.fold}', color='green')
+
+    # Parameters
     version = 0
+    fold_index = int(args.fold)
+    model_name = f'unet_baseline_f{fold_index}'
 
     # Set data directory
-    train_dir = join(hlp.DATA_DIR, 'MICCAI_BraTS2020_TrainingData')
+    data_dir = hlp.DATA_DIR
     tb_dir = join(hlp.LOG_DIR, 'tb_logs')
     snap_dir = join(hlp.LOG_DIR, 'snapshots', model_name)
     result_dir = join(hlp.LOG_DIR, 'results', model_name)
-    set_dir(train_dir, tb_dir, snap_dir, result_dir)
+    set_dir(data_dir, tb_dir, snap_dir, result_dir)
 
     # Initialize model
     log(f"Initializing <{model_name}> model")
@@ -64,13 +72,13 @@ if __name__ == '__main__':
                  hd_metric, hd_et, hd_tc, hd_wt],
 
         # Optimizer
-        optimizer=optim.AdamW,  # TODO: Why AdamW?
-        optimizer_params={'lr': 1e-4, 'weight_decay': 1e-2},
+        optimizer=optim.AdamW,
+        optimizer_params={'lr': 1e-4, 'weight_decay': 1e-5},
 
         # Learning rate scheduler
-        scheduler=WarmupCosineSchedule,
-        scheduler_config={'interval': 'step'},
-        scheduler_params={'warmup_steps': 3*3e2, 'total_steps': 1e5},
+        scheduler=CosineAnnealingWarmRestarts,
+        scheduler_config={'interval': 'epoch'},
+        scheduler_params={'T_0': 50, 'eta_min':3e-5},
 
         # Inference method
         inference=val_inference,
@@ -78,18 +86,19 @@ if __name__ == '__main__':
 
         # Test inference method
         test_inference=test_inference,
-        test_inference_params=None,
+        test_inference_params={'overlap':.5},
     )
 
     # Load checkpoint
-    # model.load_from_checkpoint(checkpoint_path=join(snap_dir,'epoch=1.ckpt'))
+    #model.load_from_checkpoint(checkpoint_path=join(snap_dir,f'final_{model_name}_v0.ckpt'))
+    #model.load_from_checkpoint(checkpoint_path=join(snap_dir, f'final_{model_name}_v{version}_fold{fold_index}.ckpt'))
 
     # Initialize data module
     log("Initializing data module")
-    brats = BraTSDataModule(data_dir=train_dir,
+    brats = BraTSDataModule(data_dir=join(data_dir,"MICCAI_BraTS2020_TrainingData"),
                             num_workers=8,
                             batch_size=1,
-                            validation_size=.2)
+                            fold_index=fold_index)
     brats.setup()
 
     # Initialize logger
@@ -98,40 +107,35 @@ if __name__ == '__main__':
     # Initialize trainer
     log("Initializing trainer")
     trainer = Trainer(
-        max_steps=100000,
-        max_epochs=100,
+        max_epochs=500,
         logger=tb_logger,
-        gpus=1,
-        num_nodes=1,
-        # distributed_backend='ddp',
+        gpus=-1,
+        #num_nodes=8,
+        deterministic=True,
+        #distributed_backend='ddp',
         callbacks=[
             LearningRateMonitor(logging_interval="step"),
             PrintTableMetricsCallback(),
-            TakeSnapshot(epochs=(1, 24, 49), save_dir=snap_dir)
+            #TakeSnapshot(epochs=(1, 24, 49), save_dir=snap_dir)
         ],
     )
 
-    # Load checkpoint (just in case)
-    model.load_from_checkpoint(join(snap_dir, f'final_{model_name}_v{version}.ckpt'))
-
     # Test
-    torch.cuda.empty_cache()
     log("Evaluating model")
     trainer.test(model=model,
+                 ckpt_path=join(snap_dir, f'final_{model_name}_v{version}_fold{fold_index}.ckpt'),
                  datamodule=brats)
     results = model.test_results
 
     # Adding model parameters
-    with torch.cuda.device(0):
+    '''with torch.cuda.device(0):
         macs, params = get_model_complexity_info(model=model, input_res=(4, 128, 128, 128), as_strings=True,
-                                                 print_per_layer_stat=True, verbose=False)
+                                                 print_per_layer_stat=True, verbose=False)'''
 
     eval = {'model_name': model_name,
             'version': version,
             'results': results,
-            'n_param': model.get_n_parameters(),
-            'flops_count': macs,
-            'params': params}
+            'n_param': model.get_n_parameters()}
 
     # Save test results
-    np.save(file=join(result_dir, f'{model_name}_v{version}.npy'), arr=results)
+    np.save(file=join(result_dir, f'{model_name}_v{version}_fold{fold_index}.npy'), arr=results)
