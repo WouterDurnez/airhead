99c9f59f1f0cbc768801ace7d6f238eb81c311d9
Wouter Durnez <wouter.durnez@gmail.com>
2021-12-06 20:35:05 +0100

    :zap: Update configs

--- configs/configs.py
+++ configs/configs.py
@@ -1,11 +1,21 @@
+#     ___   _     __               __
+#    / _ | (_)___/ /  ___ ___ ____/ /
+#   / __ |/ / __/ _ \/ -_) _ `/ _  /
+#  /_/ |_/_/_/ /_//_/\__/\_,_/\_,_/
+#
+
+'''
+Configuration for HPC training
+'''
+
 from copy import deepcopy
 
 from pl_bolts.callbacks import PrintTableMetricsCallback
 from pytorch_lightning.callbacks import LearningRateMonitor
 from torch.optim import AdamW
 
-from layers.air_conv import AirDoubleConvBlock, AirResBlock
-from layers.base_layers import DoubleConvBlock, ResBlock
+from layers.air_conv import AirResBlock
+from layers.base_layers import ResBlock
 from models.air_unet import AirUNet
 from models.base_unet import UNet
 from training.inference import test_inference
@@ -23,7 +33,7 @@ GENERAL = {
             dice_wt,
             hd_et,
             hd_tc,
-            hd_wt
+            hd_wt,
         ],
         'optimizer': AdamW,
         'optimizer_params': {
@@ -35,36 +45,42 @@ GENERAL = {
             'interval': 'step'
         },
         'scheduler_params': {
-            'warmup_steps': 0,
-            'total_steps': 100000
+            'warmup_steps': 2000,
+            'total_steps': 100000,
         },
         'inference': test_inference,
         'inference_params': {
-            'overlap': .5
+            'overlap': 0.5
         },
         'test_inference': test_inference,
         'test_inference_params': {
-            'overlap': .5
-        }
+            'overlap': 0.5
+        },
     },
     'data': {
         'root_dir': '../data/Task01_BrainTumour',
-        'num_workers': 0,
+        'spatial_size': (128, 128, 128),
+        'num_splits': 5,
+        # 'split': 0,
         'batch_size': 1,
-        'cache_rate': .1
+        'num_workers': 0,
+        # 'cache_num': 4,
+        'cache_rate': 1,
+        'seed': 616,
     },
     'training': {
-        'max_epochs': 500,
-        'gpus': 0,
+        'max_epochs': -1,  # -> sys.maxsize (which works for any #steps)
+        'gpus': 2,  # -> 2 (I usually train with 2 gpus to have a batch size of 2, 1 on each gpu)
         'deterministic': True,
         'callbacks': [
-            LearningRateMonitor(logging_interval="step"),
+            LearningRateMonitor(logging_interval='step'),
+            # ModelCheckpoint(every_n_train_steps=10), (10 is sufficiently small; the default (=1) may be too small!)
             PrintTableMetricsCallback(),
         ],
+        'check_val_every_n_epoch': 25,
     },
     'logs': {
-        'log_dir': '../logs'
-    }
+        'log_dir': '../logs'},
 }
 
 BASE_MODEL = {
@@ -73,20 +89,21 @@ BASE_MODEL = {
         'core_block': ResBlock,
         'in_channels': 4,
         'out_channels': 3,
-        'widths': (32, 64, 128, 256, 320),
-        'head': False},
+        'widths': (32, 64, 128, 256, 512),
+        'head': False,
+    },
 }
 AIR_MODEL = {
     'network': AirUNet,
     'network_params': {
         'core_block': AirResBlock,
         'core_block_conv_params': {
-            'comp_friendly': True
-        },
+            'comp_friendly': True},
         'in_channels': 4,
         'out_channels': 3,
-        'widths': (32, 64, 128, 256, 320),
-        'head': False},
+        'widths': (32, 64, 128, 256, 512),
+        'head': False,
+    },
 }
 
 CONFIG_BASE = deepcopy(GENERAL)
